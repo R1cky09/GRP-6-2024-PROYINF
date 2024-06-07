7da3554c5f09fc3e42ffea9afb4d159459796a6f
R1cky09 <93794058+R1cky09@users.noreply.github.com>
2024-06-06 21:33:58 -0400

    boton contraste
    boton contraste

--- code/src/components/DicomVista.js
+++ code/src/components/DicomVista.js
@@ -1,37 +1,39 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import * as cornerstone from 'cornerstone-core';
 import * as cornerstoneWADOImageLoader from 'cornerstone-wado-image-loader';
 import * as dicomParser from 'dicom-parser';
 
 const DicomViewer = ({ file }) => {
     const dicomImageRef = useRef(null);
+    const [invert, setInvert] = useState(false);  // Estado para controlar la inversión de la imagen
 
     useEffect(() => {
         cornerstoneWADOImageLoader.external.dicomParser = dicomParser;
         cornerstoneWADOImageLoader.external.cornerstone = cornerstone;
 
-        function onImageRendered(e) {
-            console.log('Image rendered:', e.detail);
-        }
-
         const element = dicomImageRef.current;
         if (element) {
             cornerstone.enable(element);
-            cornerstone.events.addEventListener('cornerstoneimagerendered', onImageRendered);
 
             const imageId = cornerstoneWADOImageLoader.wadouri.fileManager.add(file);
             cornerstone.loadImage(imageId).then((image) => {
-                cornerstone.displayImage(element, image);
+                const viewport = cornerstone.getDefaultViewportForImage(element, image);
+                viewport.invert = invert;  // Configura la inversión basada en el estado
+                cornerstone.displayImage(element, image, viewport);
             });
 
             return () => {
-                cornerstone.events.removeEventListener('cornerstoneimagerendered', onImageRendered);
                 cornerstone.disable(element);
             };
         }
-    }, [file]);
+    }, [file, invert]);  // Añade 'invert' a la lista de dependencias para reaccionar a los cambios
 
-    return <div ref={dicomImageRef} style={{ width: '512px', height: '512px' }} />;
+    return (
+        <div>
+            <div ref={dicomImageRef} style={{ width: '512px', height: '512px' }} />
+            <button onClick={() => setInvert(!invert)}>Invertir Colores</button>  // Botón para invertir colores
+        </div>
+    );
 };
 
 export default DicomViewer;
