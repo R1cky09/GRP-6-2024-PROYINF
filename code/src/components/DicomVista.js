a33db8362ec0c19beaa6825fde216d574926d1df
R1cky09 <93794058+R1cky09@users.noreply.github.com>
2024-10-02 11:46:12 -0300

    Revert "reordenamiento,register y login"
    This reverts commit 7eca5194bae943a711bfa3b5132ab1fa6130cbf4.

---
+++ code/src/components/DicomVista.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect, useRef } from 'react';
+import * as cornerstone from 'cornerstone-core';
+import * as cornerstoneWADOImageLoader from 'cornerstone-wado-image-loader';
+import * as dicomParser from 'dicom-parser';
+
+const DicomViewer = ({ file }) => {
+    const dicomImageRef = useRef(null);
+    const [colorMode, setColorMode] = useState('normal');  // Estado para el modo de color
+    const [contrast, setContrast] = useState(0); // Valor inicial del contraste
+    const [brightness, setBrightness] = useState(0); // Valor inicial del brillo
+
+    useEffect(() => {
+        cornerstoneWADOImageLoader.external.dicomParser = dicomParser;
+        cornerstoneWADOImageLoader.external.cornerstone = cornerstone;
+
+        const element = dicomImageRef.current;
+        if (element) {
+            cornerstone.enable(element);
+
+            const imageId = cornerstoneWADOImageLoader.wadouri.fileManager.add(file);
+            cornerstone.loadImage(imageId).then((image) => {
+                const viewport = cornerstone.getDefaultViewportForImage(element, image);
+                // Configura el viewport para el contraste y el brillo
+                viewport.voi.windowWidth = Math.max(1, image.width / 2 + contrast * 2); // Ajuste de contraste
+                viewport.voi.windowCenter = image.width / 2 + brightness; // Ajuste de brillo
+                viewport.invert = (colorMode === 'invertido'); // Ajuste de inversión
+
+                cornerstone.displayImage(element, image, viewport);
+            });
+
+            return () => {
+                cornerstone.disable(element);
+            };
+        }
+    }, [file, contrast, brightness, colorMode]); 
+
+    return (
+        <div>
+            <div className="dicomImageContainer" ref={dicomImageRef} />
+            <label>Contraste:</label>
+            <input
+                type="range"
+                min="-100"
+                max="100"
+                value={contrast}
+                onChange={(e) => setContrast(parseInt(e.target.value))}
+            />
+
+            <label>Brillo:</label>
+            <input
+                type="range"
+                min="-100"
+                max="100"
+                value={brightness}
+                onChange={(e) => setBrightness(parseInt(e.target.value))}
+            />
+            <select onChange={(e) => setColorMode(e.target.value)}>
+                <option value="normal">Normal</option>
+                <option value="invertido">Invertido</option>
+            </select>
+        </div>
+    );
+};
+
+export default DicomViewer;
